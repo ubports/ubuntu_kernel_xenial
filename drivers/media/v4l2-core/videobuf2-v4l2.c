f5d8597e3310a47e53ab6dba5e41d2cb53bcffa8
Hans Verkuil <hans.verkuil@cisco.com>
2015-04-21 14:58:46 +0200

    videodev2.h: Add request field to v4l2_buffer
    When queuing buffers allow for passing the request ID that
    should be associated with this buffer. Split the u32 reserved2 field
    into two u16 fields, one for request, one with the old reserved2 name.
    
    Change-Id: Id9e1901f2814bf618a88b1457ace6b79994759d3
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/media/v4l2-core/videobuf2-v4l2.c
+++ drivers/media/v4l2-core/videobuf2-v4l2.c
@@ -199,7 +199,7 @@ static int __fill_v4l2_buffer(struct vb2_buffer *vb, void *pb)
 	b->timestamp = vbuf->timestamp;
 	b->timecode = vbuf->timecode;
 	b->sequence = vbuf->sequence;
-	b->reserved2 = 0;
+	b->request = vbuf->request;
 	b->reserved = 0;
 
 	if (q->is_multiplanar) {
@@ -316,6 +316,7 @@ static int __fill_vb2_buffer(struct vb2_buffer *vb,
 	vbuf->timestamp.tv_sec = 0;
 	vbuf->timestamp.tv_usec = 0;
 	vbuf->sequence = 0;
+	vbuf->request = b->request;
 
 	if (V4L2_TYPE_IS_MULTIPLANAR(b->type)) {
 		if (b->memory == VB2_MEMORY_USERPTR) {
