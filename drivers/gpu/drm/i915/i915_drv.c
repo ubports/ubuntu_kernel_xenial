aefe1b44425a34ba042956651a344d6686726e06
Patrik Jakobsson <patrik.jakobsson@linux.intel.com>
2015-11-16 16:20:01 +0100

    drm/i915/gen9: Turn DC handling into a power well
    Handle DC off as a power well where enabling the power well will prevent
    the DMC to enter selected DC states (required around modesets and Aux
    A). Disabling the power well will allow DC states again. For now the
    highest DC state is DC6 for Skylake and DC5 for Broxton but will be
    configurable for Skylake in a later patch.
    
    v2: Check both DC5 and DC6 bits in power well enabled function (Ville)
    v3:
    - Remove unneeded DC_OFF case in skl_set_power_well() (Imre)
    - Add PW2 dependency to DC_OFF (Imre)
    v4: Put DC_OFF before PW2 in BXT power well array
    
    Signed-off-by: Patrik Jakobsson <patrik.jakobsson@linux.intel.com>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    [fixed line over 80 and parenthesis alignment checkpatch warns (imre)]
    Link: http://patchwork.freedesktop.org/patch/msgid/1447687201-24759-1-git-send-email-patrik.jakobsson@linux.intel.com
    
    (cherry picked from commit 9f836f9016ad5320e0c9230419d2102cf15a28aa)
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1068,9 +1068,6 @@ static int i915_pm_resume(struct device *dev)
 
 static int skl_suspend_complete(struct drm_i915_private *dev_priv)
 {
-	if (dev_priv->csr.dmc_payload)
-		skl_enable_dc6(dev_priv);
-
 	return 0;
 }
 
@@ -1115,9 +1112,6 @@ static int bxt_resume_prepare(struct drm_i915_private *dev_priv)
 
 static int skl_resume_prepare(struct drm_i915_private *dev_priv)
 {
-	if (dev_priv->csr.dmc_payload)
-		skl_disable_dc6(dev_priv);
-
 	return 0;
 }
 
--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -13350,6 +13350,9 @@ static int intel_atomic_commit(struct drm_device *dev,
 			to_intel_crtc_state(crtc->state)->update_pipe;
 		unsigned long put_domains = 0;
 
+		if (modeset)
+			intel_display_power_get(dev_priv, POWER_DOMAIN_MODESET);
+
 		if (modeset && crtc->state->active) {
 			update_scanline_offset(to_intel_crtc(crtc));
 			dev_priv->display.crtc_enable(crtc);
@@ -13373,6 +13376,9 @@ static int intel_atomic_commit(struct drm_device *dev,
 			modeset_put_power_domains(dev_priv, put_domains);
 
 		intel_post_plane_update(intel_crtc);
+
+		if (modeset)
+			intel_display_power_put(dev_priv, POWER_DOMAIN_MODESET);
 	}
 
 	/* FIXME: add subpixel order */
