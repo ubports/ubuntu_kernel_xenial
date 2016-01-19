26b0a010ec523d139d2f7e76bdcac4efd9252566
Paulo Zanoni <paulo.r.zanoni@intel.com>
2016-01-19 11:35:52 -0200

    drm/i915/fbc: make FBC work with fastboot
    Move intel_fbc_enable to a place where it is called regardless of the
    "modeset" variable, and make sure intel_fbc_enable can be called
    multiple times without intel_fbc_disable being called.
    
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1453210558-7875-20-git-send-email-paulo.r.zanoni@intel.com
    (cherry picked from commit 49227c4ae3d2c8ccb6c71bfb5dfac80ad8c73021)
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -13570,7 +13570,6 @@ static int intel_atomic_commit(struct drm_device *dev,
 		if (modeset && crtc->state->active) {
 			update_scanline_offset(to_intel_crtc(crtc));
 			dev_priv->display.crtc_enable(crtc);
-			intel_fbc_enable(intel_crtc);
 		}
 
 		if (update_pipe) {
@@ -13582,6 +13581,9 @@ static int intel_atomic_commit(struct drm_device *dev,
 
 		if (!modeset)
 			intel_pre_plane_update(intel_crtc);
+
+		if (crtc->state->active && intel_crtc->atomic.update_fbc)
+			intel_fbc_enable(intel_crtc);
 
 		if (crtc->state->active &&
 		    (crtc->state->planes_changed || update_pipe))
