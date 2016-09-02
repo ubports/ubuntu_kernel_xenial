e24878d9d405808bad56e707f483322215d0694f
Colin Ian King <colin.king@canonical.com>
2016-09-02 19:37:55 +0100

    UBUNTU: SAUCE: tsc: make no_timer_check default to 1 for AWS
    For AWS we can default the no_timer_check to be enabled to save
    ~25 milliseconds on boot.  Also allow no_timer_check=0 to
    restore this back to the original default of 0 if required.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- arch/x86/kernel/apic/io_apic.c
+++ arch/x86/kernel/apic/io_apic.c
@@ -1598,14 +1598,20 @@ void __init setup_ioapic_ids_from_mpc(void)
 }
 #endif
 
-int no_timer_check __initdata;
+int no_timer_check __initdata = 1;
 
 static int __init notimercheck(char *s)
 {
-	no_timer_check = 1;
-	return 1;
+	int ret = 0;
+
+	if (s)
+		ret = kstrtoint(s, 0, &no_timer_check);
+	else
+		no_timer_check = 1;
+
+	return ret;
 }
-__setup("no_timer_check", notimercheck);
+early_param("no_timer_check", notimercheck);
 
 /*
  * There is a nasty bug in some older SMP boards, their mptable lies
