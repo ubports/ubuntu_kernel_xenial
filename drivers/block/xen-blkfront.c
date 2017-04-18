d6a1e0ab4a3212cd3579cdef71d120d96160470b
Marc Olson <marcolso@amazon.com>
2017-04-18 19:50:00 +0200

    UBUNTU: SAUCE: blkfront: add uevent for size change
    BugLink: https://bugs.launchpad.net/bugs/1683878
    
    When a blkfront device is resized from dom0, emit a KOBJ_CHANGE uevent to
    notify the guest about the change. This allows for custom udev rules, such
    as automatically resizing a filesystem, when an event occurs.
    
    Signed-off-by: Marc Olson <marcolso@amazon.com>
    Reference: https://lkml.org/lkml/2017/4/11/736
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/block/xen-blkfront.c
+++ drivers/block/xen-blkfront.c
@@ -1996,6 +1996,7 @@ static void blkfront_connect(struct blkfront_info *info)
 	unsigned long sector_size;
 	unsigned int physical_sector_size;
 	unsigned int binfo;
+	char *envp[] = { "RESIZE=1", NULL };
 	int err;
 
 	switch (info->connected) {
@@ -2012,6 +2013,8 @@ static void blkfront_connect(struct blkfront_info *info)
 		       sectors);
 		set_capacity(info->gd, sectors);
 		revalidate_disk(info->gd);
+		kobject_uevent_env(&disk_to_dev(info->gd)->kobj,
+				   KOBJ_CHANGE, envp);
 
 		return;
 	case BLKIF_STATE_SUSPENDED:
