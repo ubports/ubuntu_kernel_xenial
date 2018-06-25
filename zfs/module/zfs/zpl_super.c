01d0aa14c4cc2ff5792b0a729aa4e91212d9679e
Colin Ian King <colin.king@canonical.com>
2018-06-25 21:36:00 +0200

    UBUNTU: SAUCE: Allow mounting datasets more than once (LP: #1759848)
    BugLink: https://bugs.launchpad.net/bugs/1759848
    
    Currently mounting an already mounted zfs dataset results in an
    error, whereas it is typically allowed with other filesystems.
    This causes some bad interactions with mount namespaces. Take
    this sequence for example:
    
     - Create a dataset
     - Create a snapshot of the dataset
     - Create a clone of the snapshot
     - Create a new mount namespace
     - Rename the original dataset
    
    The rename results in unmounting and remounting the clone in the
    original mount namespace, however the remount fails because the
    dataset is still mounted in the new mount namespace. (Note that
    this means the mount in the new mount namespace is never being
    unmounted, so perhaps the unmount/remount of the clone isn't
    actually necessary.)
    
    The problem here is a result of the way mounting is implemented
    in the kernel module. Since it is not mounting block devices it
    uses mount_nodev() instead of the usual mount_bdev(). However,
    mount_nodev() is written for filesystems for which each mount is
    a new instance (i.e. a new super block), and zfs should be able
    to detect when a mount request can be satisfied using an existing
    super block.
    
    Change zpl_mount() to call sget() directly with it's own test
    callback. Passing the objset_t object as the fs data allows
    checking if a superblock already exists for the dataset, and in
    that case we just need to return a new reference for the sb's
    root dentry.
    
    [ Sync'd from zfsutils-linux, from a patch by Seth Forshee and
      backported to zfs 0.6.5.6. Note that this also contains
      some zfstutils changes between 0.6.5.6-0ubuntu20 and
      0.6.5.6-0ubuntu24 which go also sync'd into this fix, which
      is expected part of the zfs sync'ing ]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- zfs/module/zfs/zpl_super.c
+++ zfs/module/zfs/zpl_super.c
@@ -443,12 +443,56 @@ zpl_fill_super(struct super_block *sb, void *data, int silent)
 	return (error);
 }
 
-#ifdef HAVE_MOUNT_NODEV
+static int
+zpl_test_super(struct super_block *s, void *data)
+{
+	zfs_sb_t *zsb = s->s_fs_info;
+
+	objset_t *os = data;
+
+	if (zsb == NULL)
+		return (0);
+
+	return (os == zsb->z_os);
+}
+
+static struct super_block *
+zpl_mount_impl(struct file_system_type *fs_type, int flags, zfs_mntopts_t *zmo)
+{
+	struct super_block *s;
+	objset_t *os;
+	int err;
+
+	err = dmu_objset_hold(zmo->z_osname, FTAG, &os);
+	if (err)
+		return (ERR_PTR(-err));
+
+	s = zpl_sget(fs_type, zpl_test_super, set_anon_super, flags, os);
+	dmu_objset_rele(os, FTAG);
+	if (IS_ERR(s))
+		return (ERR_CAST(s));
+
+	if (s->s_root == NULL) {
+		err = zpl_fill_super(s, zmo, flags & SB_SILENT ? 1 : 0);
+		if (err) {
+			deactivate_locked_super(s);
+			return (ERR_PTR(err));
+		}
+		s->s_flags |= SB_ACTIVE;
+	} else if ((flags ^ s->s_flags) & SB_RDONLY) {
+		deactivate_locked_super(s);
+		return (ERR_PTR(-EBUSY));
+	}
+
+	return (s);
+}
+#ifdef HAVE_FST_MOUNT
 static struct dentry *
 zpl_mount(struct file_system_type *fs_type, int flags,
     const char *osname, void *data)
 {
 	zfs_mntopts_t *zmo = zfs_mntopts_alloc();
+	struct super_block *sb;
 	int error;
 
 	error = zpl_parse_options((char *)osname, (char *)data, zmo, B_FALSE);
@@ -456,8 +500,11 @@ zpl_mount(struct file_system_type *fs_type, int flags,
 		zfs_mntopts_free(zmo);
 		return (ERR_PTR(error));
 	}
+	sb = zpl_mount_impl(fs_type, flags, zmo);
+	if (IS_ERR(sb))
+		return (ERR_CAST(sb));
 
-	return (mount_nodev(fs_type, flags, zmo, zpl_fill_super));
+	return (dget(sb->s_root));
 }
 #else
 static int
@@ -465,6 +512,7 @@ zpl_get_sb(struct file_system_type *fs_type, int flags,
     const char *osname, void *data, struct vfsmount *mnt)
 {
 	zfs_mntopts_t *zmo = zfs_mntopts_alloc();
+	struct super_block *sb;
 	int error;
 
 	error = zpl_parse_options((char *)osname, (char *)data, zmo, B_FALSE);
@@ -472,10 +520,15 @@ zpl_get_sb(struct file_system_type *fs_type, int flags,
 		zfs_mntopts_free(zmo);
 		return (error);
 	}
+	sb = zpl_mount_impl(fs_type, flags, zmo);
+	if (IS_ERR(sb))
+		return (ERR_CAST(sb));
 
-	return (get_sb_nodev(fs_type, flags, zmo, zpl_fill_super, mnt));
+	(void) simple_set_mnt(mnt, sb);
+
+	return (0);
 }
-#endif /* HAVE_MOUNT_NODEV */
+#endif /* HAVE_FST_MOUNT */
 
 static void
 zpl_kill_sb(struct super_block *sb)
@@ -542,10 +595,10 @@ const struct super_operations zpl_super_operations = {
 struct file_system_type zpl_fs_type = {
 	.owner			= THIS_MODULE,
 	.name			= ZFS_DRIVER,
-#ifdef HAVE_MOUNT_NODEV
+#ifdef HAVE_FST_MOUNT
 	.mount			= zpl_mount,
 #else
 	.get_sb			= zpl_get_sb,
-#endif /* HAVE_MOUNT_NODEV */
+#endif /* HAVE_FST_MOUNT */
 	.kill_sb		= zpl_kill_sb,
 };
